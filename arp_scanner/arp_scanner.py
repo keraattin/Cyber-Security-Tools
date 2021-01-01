f35247c574eeed36f409aee35a26847768d76151
keraattin <kerem-oruc@outlook.com>
2021-01-01 21:19:28 +0300

    Restful Service Mode Implemented

--- arp_scanner/arp_scanner.py
+++ arp_scanner/arp_scanner.py
@@ -1,6 +1,5 @@
-#!/usr/bin/env python
-
-
+from flask import Flask
+from flask_restful import Resource, Api
 import scapy.all as scapy
 
 
@@ -10,39 +9,40 @@ BRDCST_MAC = "ff:ff:ff:ff:ff:ff"                  # Broadcast MAC Address
 ANSWRD_LST_INDEX = 0                              # Answered Packages Index
 ARP_FRAME_INDEX = 1                               # Arp Frame Index
 TARGET = "10.0.2.1/24"                            # Target IP Address
+TIMEOUT = 1                                       # Timeout
+
+app = Flask(__name__)
+api = Api(app)
 ##############################################################################
 
 
-def scan(ip,timeout=1):
-    arp_request = scapy.ARP()                     # ARP Package Generated
-    arp_request.pdst = ip                         # Set Target IP Address
+class ArpScan(Resource):
+    def get(self):
+        arp_request = scapy.ARP()                     # ARP Package Generated
+        arp_request.pdst = TARGET                     # Set Target IP Address
 
-    broadcast_ether = scapy.Ether()               # Ethernet Frame Generated 
-    broadcast_ether.dst = BRDCST_MAC              # Set Destination MAC
+        broadcast_ether = scapy.Ether()               # Ethernet Frame Generated 
+        broadcast_ether.dst = BRDCST_MAC              # Set Destination MAC
 
-    # Arp and Broadcast Ether Package Combined
-    broadcast_arp_request = broadcast_ether/arp_request
+        # Arp and Broadcast Ether Package Combined
+        broadcast_arp_request = broadcast_ether/arp_request
 
-    # Sending Packages
-    answered_list = scapy.srp(broadcast_arp_request,
-        timeout=timeout, verbose = False)[ANSWRD_LST_INDEX]
-    
-    clients_list = []                             #List For Clients
+        # Sending Packages
+        answered_list = scapy.srp(broadcast_arp_request,
+            timeout=1, verbose = False)[ANSWRD_LST_INDEX]
+        
+        clients_list = []                             #List For Clients
 
-    for client in answered_list:
-        client_dict = {"ip_addr":client[ARP_FRAME_INDEX].psrc,
-                       "mac_addr":client[ARP_FRAME_INDEX].hwsrc}
-        clients_list.append(client_dict)
-    
-    return clients_list
-
-
-def main():
-    clients = scan(TARGET)
-    print("IP ADDRESS\tMAC ADDRESS")
-    for client in clients:
-        print(client["ip_addr"]+"\t"+client["mac_addr"])
+        for client in answered_list:
+            client_dict = {"ip_addr":client[ARP_FRAME_INDEX].psrc,
+                        "mac_addr":client[ARP_FRAME_INDEX].hwsrc}
+            clients_list.append(client_dict)
+        
+        return clients_list
 
 
-if __name__ == "__main__":
-    main()
+api.add_resource(ArpScan, '/arp_scan')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
