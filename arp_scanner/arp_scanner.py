a7e6580188aa171a161a76992c46f5724951376d
keraattin <kerem-oruc@outlook.com>
2021-01-11 20:51:24 +0300

    Default 404 Error Page Added

--- arp_scanner/arp_scanner.py
+++ arp_scanner/arp_scanner.py
@@ -102,6 +102,13 @@ class ArpScan(Resource):
 @app.route('/', methods=['GET'])
 def home():
     return render_template('home.html', port=PORT)
+
+
+## Error Page
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
 ##############################################################################
 
 
