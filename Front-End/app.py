ed6e02bb41ec7c6c20d84e5a89c94bc3bbb28942
keraattin <kerem-oruc@outlook.com>
2021-02-14 21:30:06 +0300

    Front-End Edited

--- Front-End/app.py
+++ Front-End/app.py
@@ -1,11 +1,44 @@
 #!/usr/bin/env python3
 
+
+# Imports
+##############################################################################
+from flask import Flask, request, abort, jsonify, render_template
+import requests
+##############################################################################
+
+
+# Global Values
+##############################################################################
+DEBUG = True                                      # Debug Mode
+PORT = 5000                                       # Port Number
+
+API_URL = "http://localhost:5000"                 # API Services Url
+ARP_SCN_URL = API_URL + "/api/arp_scan"           # Arp Scan API Url
+ARP_SPF_URL = API_URL + "/api/arp_spoof"          # Arp Spoof API Url
+
+app = Flask(__name__)
+##############################################################################
+
+
 # Pages
 ##############################################################################
 ## Homepage
 @app.route('/', methods=['GET'])
 def home():
-    return render_template('home.html', port=PORT)
+    return render_template('home.html')
+
+
+## Arp Scanner
+@app.route('/arp_scanner/', methods=['GET'])
+def arp_scanner():
+    return render_template('arp_scanner.html', api_url=ARP_SCN_URL)
+
+
+## Arp Spoofer
+@app.route('/arp_spoofer/', methods=['GET'])
+def arp_spoofer():
+    return render_template('arp_spoofer.html', api_url=ARP_SPF_URL)
 
 
 ## Error Page
@@ -16,10 +49,8 @@ def page_not_found(e):
 ##############################################################################
 
 
-# Pages
+# Main
 ##############################################################################
-## Homepage
-@app.route('/', methods=['GET'])
-def home():
-    return render_template('home.html', port=PORT)
+if __name__ == '__main__':
+    app.run(debug=DEBUG, port=PORT)
 ##############################################################################
