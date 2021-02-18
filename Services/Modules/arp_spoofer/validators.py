939a158ba34b2e9ec7d597a07cbca596f2ccfbd2
keraattin <kerem-oruc@outlook.com>
2021-02-18 20:16:17 +0300

    MAC Address Validation Added

--- Services/Modules/arp_spoofer/validators.py
+++ Services/Modules/arp_spoofer/validators.py
@@ -13,14 +13,27 @@ IP_PATTERN = ("^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
                     "\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
                     "\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
                     "\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$")
+
+MAC_PATTRN = ("^([0-9a-fA-F]{2}:){5}[0-9a-fA-F]{2}$")
 ##############################################################################
 
 
-# Validate Target IP Address
+# Validate IP Address
 ##############################################################################
 def validate_ip_addr(ip_addr):
     ip_matches = re.search(IP_PATTERN,ip_addr)
     if ip_matches:
+        return True
+    else:
+        return False
+##############################################################################
+
+
+# Validate Mac Address
+##############################################################################
+def validate_mac_addr(mac_addr):
+    matches = re.search(MAC_PATTRN,mac_addr)
+    if matches:
         return True
     else:
         return False
