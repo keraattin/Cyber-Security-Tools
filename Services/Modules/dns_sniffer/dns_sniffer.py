cda44a9f37f88cd4002986a9989994c6d30cc835
keraattin <kerem-oruc@outlook.com>
2021-02-25 22:59:53 +0300

    Services & Get Method Added

--- Services/Modules/dns_sniffer/dns_sniffer.py
+++ Services/Modules/dns_sniffer/dns_sniffer.py
@@ -5,6 +5,11 @@
 ##############################################################################
 from scapy.all import *
 import sys
+from flask_restful import Resource, Api, abort
+from werkzeug.exceptions import BadRequest
+from flask import Flask, request, abort, jsonify
+
+from .schema import DnsSnifferGetSchema
 ##############################################################################
 
 
@@ -13,10 +18,11 @@ import sys
 INTERFACE = "eth0"
 A_RECORD  = 1
 FILTER    = "port 53"
+CACHE     = []
 ##############################################################################
 
 
-
+#Functions
 ##############################################################################
 # DNS Question Records
 def sniff_dns_qr(packet):
@@ -25,7 +31,9 @@ def sniff_dns_qr(packet):
         if packet.haslayer(DNSQR):
             if packet.getlayer(DNSQR).qtype == A_RECORD:
                 qname = packet.getlayer(DNSQR).qname.decode("utf-8")
-                print("["+ ip_src +"] \t-> [" + qname + "]")
+                qr = { "ip_src" : ip_src,
+                       "qname"  : qname }
+                CACHE.append(qr)
 
 
 # DNS Resource Records
@@ -36,9 +44,29 @@ def sniff_dns_rr(packet):
             if packet.getlayer(DNSRR).type == A_RECORD:
                 rrname = packet.getlayer(DNSRR).rrname.decode("utf-8")
                 rdata  = packet.getlayer(DNSRR).rdata
-                print("["+ ip_src +"] \t-> [" + rrname + "(" + rdata + ")]")
+                rr     = { "ip_src" : ip_src,
+                           "rrname" : rrname }
+                CACHE.append(rr)
 ##############################################################################
 
+
+# Api Class
+##############################################################################
+class DnsSniffer(Resource):
+    def get(self):
+        dns_sniffer_get_schema = DnsSnifferGetSchema()        # Schema Object
+
+        data = request.get_json()                             # Getting Data
+
+        # Validating Request Parameters
+        errors = dns_sniffer_get_schema.validate(data)
+        if errors:
+            raise BadRequest(errors)
+
+        iface  = data['iface']
+
+        return CACHE
+##############################################################################
 
 
 ##############################################################################
